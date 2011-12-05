baf9d215330c69551ce77aaad01150a897714170
Michael Brodhead <mkb@engineyard.com>
2011-12-05 15:31:28 -0800

    real test now

--- lib/excon/connection.rb
+++ lib/excon/connection.rb
@@ -68,114 +68,115 @@ module Excon
     #     @option params [Hash]   :query appended to the 'scheme://host:port/path/' in the form of '?key=value'
     #     @option params [String] :scheme The protocol; 'https' causes OpenSSL to be used
     def request(params, &block)
-      begin
-        # connection has defaults, merge in new params to override
-        params = @connection.merge(params)
-        params[:headers] = @connection[:headers].merge(params[:headers] || {})
-        params[:headers]['Host'] ||= '' << params[:host] << ':' << params[:port]
+      ActiveSupport::Notifications.instrument('excon.request') do
+        begin
+          # connection has defaults, merge in new params to override
+          params = @connection.merge(params)
+          params[:headers] = @connection[:headers].merge(params[:headers] || {})
+          params[:headers]['Host'] ||= '' << params[:host] << ':' << params[:port]
 
-        # if path is empty or doesn't start with '/', insert one
-        unless params[:path][0, 1] == '/'
-          params[:path].insert(0, '/')
-        end
+          # if path is empty or doesn't start with '/', insert one
+          unless params[:path][0, 1] == '/'
+            params[:path].insert(0, '/')
+          end
 
-        unless params[:mock]
-          socket.params = params
-        else
-          mocked_response = invoke_stub(params, &block)
-          return mocked_response unless mocked_response.nil?
-        end
+          unless params[:mock]
+            socket.params = params
+          else
+            mocked_response = invoke_stub(params, &block)
+            return mocked_response unless mocked_response.nil?
+          end
 
-        # start with "METHOD /path"
-        request = params[:method].to_s.upcase << ' '
-        if @proxy
-          request << params[:scheme] << '://' << params[:host] << ':' << params[:port]
-        end
-        request << params[:path]
+          # start with "METHOD /path"
+          request = params[:method].to_s.upcase << ' '
+          if @proxy
+            request << params[:scheme] << '://' << params[:host] << ':' << params[:port]
+          end
+          request << params[:path]
 
-        # add query to path, if there is one
-        case params[:query]
-        when String
-          request << '?' << params[:query]
-        when Hash
-          request << '?'
-          for key, values in params[:query]
-            if values.nil?
-              request << key.to_s << '&'
+          # add query to path, if there is one
+          case params[:query]
+          when String
+            request << '?' << params[:query]
+          when Hash
+            request << '?'
+            for key, values in params[:query]
+              if values.nil?
+                request << key.to_s << '&'
+              else
+                for value in [*values]
+                  request << key.to_s << '=' << CGI.escape(value.to_s) << '&'
+                end
+              end
+            end
+            request.chop! # remove trailing '&'
+          end
+
+          # finish first line with "HTTP/1.1\r\n"
+          request << HTTP_1_1
+
+          # calculate content length and set to handle non-ascii
+          unless params[:headers].has_key?('Content-Length')
+            params[:headers]['Content-Length'] = case params[:body]
+            when File
+              params[:body].binmode
+              File.size(params[:body])
+            when String
+              if FORCE_ENC
+                params[:body].force_encoding('BINARY')
+              end
+              params[:body].length
             else
-              for value in [*values]
-                request << key.to_s << '=' << CGI.escape(value.to_s) << '&'
+              0
+            end
+          end
+
+          # add headers to request
+          for key, values in params[:headers]
+            for value in [*values]
+              request << key.to_s << ': ' << value.to_s << CR_NL
+            end
+          end
+
+          # add additional "\r\n" to indicate end of headers
+          request << CR_NL
+
+          # write out the request, sans body
+          socket.write(request)
+
+          # write out the body
+          if params[:body]
+            if params[:body].is_a?(String)
+              socket.write(params[:body])
+            else
+              while chunk = params[:body].read(CHUNK_SIZE)
+                socket.write(chunk)
               end
             end
           end
-          request.chop! # remove trailing '&'
-        end
 
-        # finish first line with "HTTP/1.1\r\n"
-        request << HTTP_1_1
+          # read the response
+          response = Excon::Response.parse(socket, params, &block)
 
-        # calculate content length and set to handle non-ascii
-        unless params[:headers].has_key?('Content-Length')
-          params[:headers]['Content-Length'] = case params[:body]
-          when File
-            params[:body].binmode
-            File.size(params[:body])
-          when String
-            if FORCE_ENC
-              params[:body].force_encoding('BINARY')
-            end
-            params[:body].length
-          else
-            0
+          if response.headers['Connection'] == 'close'
+            reset
           end
-        end
 
-        # add headers to request
-        for key, values in params[:headers]
-          for value in [*values]
-            request << key.to_s << ': ' << value.to_s << CR_NL
-          end
-        end
-
-        # add additional "\r\n" to indicate end of headers
-        request << CR_NL
-
-        # write out the request, sans body
-        socket.write(request)
-
-        # write out the body
-        if params[:body]
-          if params[:body].is_a?(String)
-            socket.write(params[:body])
-          else
-            while chunk = params[:body].read(CHUNK_SIZE)
-              socket.write(chunk)
-            end
-          end
-        end
-
-        # read the response
-        response = Excon::Response.parse(socket, params, &block)
-
-        if response.headers['Connection'] == 'close'
+          response
+        rescue Excon::Errors::StubNotFound => stub_not_found
+          raise(stub_not_found)
+        rescue => socket_error
           reset
+          raise(Excon::Errors::SocketError.new(socket_error))
         end
 
-        response
-      rescue Excon::Errors::StubNotFound => stub_not_found
-        raise(stub_not_found)
-      rescue => socket_error
-        reset
-        raise(Excon::Errors::SocketError.new(socket_error))
+        if params.has_key?(:expects) && ![*params[:expects]].include?(response.status)
+          reset
+          raise(Excon::Errors.status_error(params, response))
+        else
+          response
+        end
       end
-
-      if params.has_key?(:expects) && ![*params[:expects]].include?(response.status)
-        reset
-        raise(Excon::Errors.status_error(params, response))
-      else
-        response
-      end
-
     rescue => request_error
       if params[:idempotent] && [Excon::Errors::SocketError, Excon::Errors::HTTPStatusError].any? {|ex| request_error.kind_of? ex }
         retries_remaining ||= retry_limit
@@ -192,7 +193,7 @@ module Excon
         raise(request_error)
       end
     end
-    
+
     def invoke_stub(params)
       for stub, response in Excon.stubs
         # all specified non-headers params match and no headers were specified or all specified headers match
--- tests/instrumentation_tests.rb
+++ tests/instrumentation_tests.rb
@@ -1,20 +1,34 @@
 require 'active_support/notifications'
 
 Shindo.tests('Instrumentation of connections') do
+  # Excon.mock = true
+
+  before do
+    @events = []
+  end
+
+  after do
+    # flush any existing stubs after each test
+    Excon.stubs.clear
+  end
+
   with_rackup('request_methods.ru') do
-    tests('nada').returns([:cheezburger]) do
-      
-      @notifications = []
-      
-      ActiveSupport::Notifications.subscribe(/cheezburger/) do |event|
-        @notifications << event
+    tests('basic notification').returns('excon.request') do
+      ActiveSupport::Notifications.subscribe(/excon/) do |*args|
+        @events << ActiveSupport::Notifications::Event.new(*args)
       end
-      
-      ActiveSupport::Notifications.instrument(:cheezburger) do
-        Excon.get('http://localhost:9292').body
-      end
-      
-      @notifications
+
+      Excon.get('http://localhost:9292').body
+
+      @events.first.name
     end
   end
+
+    tests('notify on retry')
+    tests('notify on error')
+    tests('filtering')
+    tests('indicates duration')
+    # excon.request
+    # excon.error
+    # excon.retry
 end
